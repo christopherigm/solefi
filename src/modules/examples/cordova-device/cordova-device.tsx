8c960a3f940bede9bb714663b9621bbdda7fc0ce
Christopher Guzman <christopher.guzman.monsalvo@gmail.com>
2021-04-20 21:52:48 +0000

    Add collapsible

---
+++ src/modules/examples/cordova-device/cordova-device.tsx
@@ -0,0 +1,49 @@
+import { useEffect, useRef, useState } from 'react';
+import * as M from 'materialize-css';
+
+const CordovaDevice = () => {
+  const collapsibleRef: any = useRef(null);
+  const [device, setDevice] = useState({
+    platform: '',
+    model: '',
+    uuid: ''
+  });
+
+  useEffect(() => {
+    M.Collapsible.init(collapsibleRef.current, {});
+    const w: any = window;
+    if ( w.device ) {
+      setDevice(w.device);
+      console.log(device);
+    }
+  });
+
+  return (
+    <>
+      <br /><br />
+      <p>Device information</p>
+      <ul className='collapsible' ref={collapsibleRef}>
+        <li>
+          <div className='collapsible-header'><i className='material-icons'>phone_iphone</i>Device</div>
+          <div className='collapsible-body'>
+            <span>{ device.platform ? device.platform : 'Web' }</span>
+          </div>
+        </li>
+        <li>
+          <div className='collapsible-header'><i className='material-icons'>merge_type</i>Model</div>
+          <div className='collapsible-body'>
+            <span>{ device.model ? device.model : '-' }</span>
+          </div>
+        </li>
+        <li>
+          <div className='collapsible-header'><i className='material-icons'>memory</i>UUID</div>
+          <div className='collapsible-body'>
+            <span>{ device.uuid ? device.uuid : 'none' }</span>
+          </div>
+        </li>
+      </ul>
+    </>
+  );
+};
+
+export default CordovaDevice;
