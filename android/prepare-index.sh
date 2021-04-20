d126771be9cad79ca5d9fe7f4d1617cddbd0ff72
Christopher Guzman <christopher.guzman.monsalvo@gmail.com>
2021-04-20 06:43:17 +0000

    Refactor integration

---
+++ android/prepare-index.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+sed -i 's/\/static/static/g' app/www/index.html
+
+echo 'Index ready!'
---
+++ src/pages/home/home.tsx
@@ -0,0 +1,12 @@
+import NavBar from 'src/modules/nav-bar/nav-bar';
+
+const Home = () => {
+  return (
+    <>
+      <NavBar />
+      Home
+    </>
+  );
+};
+
+export default Home;
