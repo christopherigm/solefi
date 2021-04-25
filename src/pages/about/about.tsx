a6c123e9cd4a0cc4b994f1bad44fd21b14d13e5f
Edgar Guzman <e.gm27@outlook.com>
2021-04-25 03:41:38 +0000

    Title creation

--- src/pages/about/about.tsx
+++ src/pages/about/about.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import NavBar from 'src/modules/nav-bar/nav-bar';
+import BusinessAccounting from 'src/modules/business-accounting/Bussiness';
 
 const About = (): React.ReactElement => {
   return (
     <>
       <NavBar />
       About Section
+      <BusinessAccounting/>
     </>
   );
 };
