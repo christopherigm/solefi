a1b303fd416aaf2a1e782abf915ba439907bff10
Edgar <e.gm27@outlook.com>
2021-06-23 00:13:45 +0000

    MediaVideo

---
+++ src/modules/media-video/parallax-background.tsx
@@ -0,0 +1,65 @@
+import React, {useState, useRef} from 'react';
+import 'src/modules/media-video/media-video.scss';
+import Title from 'src/modules/title/title';
+import ReactPlayer from 'react-player';
+import { useSelector } from 'react-redux';
+
+const ParallaxBackground = (props: any): React.ReactElement => {
+
+  const [showVideo, setShowVideo] = useState(false);
+  const system = useSelector((state: any) => state.system);
+  const prefix = system.platform.prefix;
+  const videoFile = '/video.mp4';
+  const videoURL = `${prefix}${videoFile}`;
+  const videoRef: any = useRef(null);
+
+  return (
+    <div className='parallax-container'>
+      <div className='parallax'>
+        <img src={props.image} alt=''/>
+      </div>
+      <div className='MediaVideo__img'>
+        <div className='container row'>
+        <div className='MediaVideo__title col s12'>
+          <Title text='Mira nuestro video corporativo' color='white'/>
+        </div>
+        <div className='col s4 offset-s4'>
+          <div
+            className='MediaVideo__btnVideo'
+            onClick={()=>{
+              videoRef.current.seekTo(0);
+              setShowVideo(true);
+            }}>
+            <div
+              className='MediaVideo__playIcon'
+              style= {{backgroundImage: `url(${props.icon})`}}>
+            </div>
+          </div>
+          <div className={`MediaVideo__wrapper valign-wrapper ${showVideo ? '' : 'hide'
+              }`} onClick={() => {
+              setShowVideo(false);
+              }}>
+          <div className='MediaVideo__videocontainer container'>
+            <ReactPlayer
+              url={videoURL}
+              width='100%'
+              height='100%'
+              playing={showVideo}
+              muted={!showVideo}
+              ref={videoRef}
+            />
+            <div
+              className={`MediaVideo__button waves-effect waves-light tbn white-text center-align  btn-floating red pulse${showVideo ? '' : 'hide'
+              }`} onClick={()=> {
+                setShowVideo(true);
+              }} >Cerrar</div>
+          </div>
+          </div>
+        </div>
+      </div>
+      </div>
+    </div>
+  );
+};
+
+export default ParallaxBackground;
