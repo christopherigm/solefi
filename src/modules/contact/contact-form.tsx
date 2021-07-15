83a4b4074e59388ad333b39433d6a9a55b1891fd
Christopher Guzman <christopher.guzman.monsalvo@gmail.com>
2021-07-15 23:14:27 +0000

    Add slide buttons

--- src/modules/contact/contact-form.tsx
+++ src/modules/contact/contact-form.tsx
@@ -3,43 +3,41 @@ import ContactItem from 'src/modules/contact/contact-item';
 import Title from 'src/modules/title/title';
 
 const ContactForm = (): React.ReactElement => {
-  const HideShadowTitle = false;
-
   return (
     <div className='ContactForm'>
-    <div>
-      <Title
-        color='white'
-        text='Solefi a un click'
-        shadow={HideShadowTitle}
-      />
-    </div>
-    <div className='container row ContactForm__info'>
-    <div className='col s12 m4 ContactForm__block'>
-      <ContactItem
-        link='https://maps.google.com/?q=Casas Grandes 245, Colonia Narvarte Oriente, 03023 Benito Juarez, Distrito Federal, Mexico./'
-        icon='location_on'
-        tile='Visítanos en'
-        text='Casas Grandes 245, Colonia Narvarte Oriente, 03023 Benito Juarez, Distrito Federal, Mexico.'
-        linkText='Click Aquí'/>
-    </div>
-    <div className='col s12 m4 ContactForm__block'>
-      <ContactItem
-        link='tel:+52 1 56 1416 8548/'
-        icon='local_phone'
-        tile='Llámanos al'
-        text='+52 1 56 1416 8548'
-        linkText='Click Aquí'/>
-    </div>
-    <div className='col s12 m4 ContactForm__block'>
-      <ContactItem
-        link='mailto:hola@solefi.com.mx'
-        icon='email'
-        tile='Escríbenos a'
-        text='hola@solefi.com.mx'
-        linkText='Click Aquí'/>
-    </div>
-    </div>
+      <div>
+        <Title
+          color='white'
+          text='Solefi a un click'
+          shadow={true}
+        />
+      </div>
+      <div className='container row ContactForm__info'>
+        <div className='col s12 m4 ContactForm__block'>
+          <ContactItem
+            link='https://maps.google.com/?q=Casas Grandes 245, Colonia Narvarte Oriente, 03023 Benito Juarez, Distrito Federal, Mexico./'
+            icon='location_on'
+            tile='Visítanos en'
+            text='Casas Grandes 245, Colonia Narvarte Oriente, 03023 Benito Juarez, Distrito Federal, Mexico.'
+            linkText='Click Aquí'/>
+        </div>
+        <div className='col s12 m4 ContactForm__block'>
+          <ContactItem
+            link='tel:56 1416 8548'
+            icon='local_phone'
+            tile='Llámanos al'
+            text='56 1416 8548'
+            linkText='Click Aquí'/>
+        </div>
+        <div className='col s12 m4 ContactForm__block'>
+          <ContactItem
+            link='mailto:hola@solefi.com.mx'
+            icon='email'
+            tile='Escríbenos a'
+            text='hola@solefi.com.mx'
+            linkText='Click Aquí'/>
+        </div>
+      </div>
     </div>
   );
 };
